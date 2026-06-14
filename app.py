d8e55ada96be4afd3e3a1af2af54c7e3267f72c1
Tianyue Zhao <tianyuezhao2022@gmail.com>
2026-06-14 01:41:45 -0400

    Describe your changes

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+#class Courses(db.Model):
+
 
 @app.route("/")
 def home():
@@ -8,10 +15,26 @@ def home():
 
 @app.route("/courses")
 def cur_courses():
-    return "Hello"
+    return render_template('index1.html')
 
 @app.route("/timetable")
 def cur_timetable():
     return "True"
+
+@app.route("/courses/MDM4U1")
+def MDM4U1():
+    return "False"
+
+@app.route("/courses/MCV4U1")
+def MCV4U1():
+    return "False"
+
+@app.route("/courses/MHF4U1")
+def MHF4U1():
+    return "False"
+
+@app.route("/courses/ICS4U1")
+def ICS4U1():
+    return "False"
 
 app.run(debug=True)
